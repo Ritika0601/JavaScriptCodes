17f04f548369ec1e84d7a41787505386f7b7e4e0
Ritika0601 <ritika.soni@affle.com>
2017-04-13 02:44:12 +0530

    update game.js

--- Snake_Ladder/Coding/game.js
+++ Snake_Ladder/Coding/game.js
@@ -1,4 +1,26 @@
 function start()
 {
+	var gameBody = document.getElementsByTagName("body")[0];
+	var divBody = document.createElement("div");
 	
+
+	for(var row=1;row<=10;row++)
+	{
+		var divGameBody = document.createElement("div");
+		for(var col=1;col<=10;col++)
+		{
+			var divChild = document.createElement("div");
+			divChild.style.cssFloat="left";
+			var box = document.createElement("input");
+			box.setAttribute("type","text");
+			box.setAttribute("value",row);
+			divChild.appendChild(box);
+			divGameBody.appendChild(divChild);
+		}
+		divChild.style.cssFloat="initial";
+        divGameBody.appendChild(divChild);
+	}
+	divBody.appendChild(divGameBody);
+	gameBody.appendChild(divBody);
+
 }
